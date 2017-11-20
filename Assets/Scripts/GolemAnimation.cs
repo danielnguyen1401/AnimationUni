12c5ff5a90d1e447fad3bd3ad419a1f7f777369a
Yuan <nguyen.leenguyen@gmail.com>
2017-11-20 16:02:50 +0700

    Animator Controller

---
+++ Assets/Scripts/GolemAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GolemAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private string SLEEP_END_ANIMATION = "sleep_end";
+    private string IDLE_ANIMATION = "idle";
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void EndSleep()
+    {
+        anim.Play(SLEEP_END_ANIMATION);
+    }
+
+    public void BeginIdle()
+    {
+        anim.Play(IDLE_ANIMATION);
+    }
+}
